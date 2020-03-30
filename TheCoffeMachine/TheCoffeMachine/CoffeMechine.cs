bd61bb176b9b2a54627dc71d3568f05f948f5e8b
whidyamaharani <62237070+whidyamaharani@users.noreply.github.com>
2020-03-30 18:42:23 +0700

    Add files via upload

---
+++ TheCoffeMachine/TheCoffeMachine/CoffeMechine.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheCoffeMachine
+{
+    class CoffeMechine
+    {
+        private CoffePowder coffePowder;
+        private WaterGalon waterGalon;
+        private Milk milk;
+
+        public CoffeMechine(CoffePowder powder, WaterGalon galon, Milk milk)
+        {
+            this.waterGalon = galon;
+            this.coffePowder = powder;
+            this.milk = milk;
+        }
+
+        public String makeCappucino()
+        {
+            if (!this.waterGalon.isAvailable())
+            {
+                return "Sorry, the coffe is empty";
+            }
+
+            this.waterGalon.makeOneCup();
+            this.coffePowder.makeOneCup();
+            this.milk.makeOneCup();
+            return "Yey! your coffe is ready";
+        }
+
+        public String checkAvailability()
+        {
+            return $"the water :{this.waterGalon.getVolume()} the coffe powder : {this.coffePowder.getNetto()} and the milk :{this.milk.getVolume()}";
+        }
+    }
+}
